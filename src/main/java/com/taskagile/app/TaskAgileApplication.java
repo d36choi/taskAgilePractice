fd0ee8f8fa1a96429040a14dba27c5684d65218a
d36choi <yuio1126@gmail.com>
2021-06-11 16:52:01 +0900

    create be scaffold

---
+++ src/main/java/com/taskagile/app/TaskAgileApplication.java
@@ -0,0 +1,13 @@
+package com.taskagile.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAgileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAgileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskagile/app/TaskAgileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskagile.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAgileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
