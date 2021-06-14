e55a1677c06518a092021e500c78faddd29c6790
d36choi <yuio1126@gmail.com>
2021-06-14 10:28:05 +0900

    create front-end scaffold

---
+++ front-end/src/router/index.js
@@ -0,0 +1,13 @@
+import LoginPage from '@/views/LoginPage'
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [{
+    path: '/login',
+    name: 'LoginPage',
+    component: LoginPage
+  }]
+})
