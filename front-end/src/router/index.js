a1d0c3e504e74c4f48a6fac31954aa4551382ecb
d36choi <yuio1126@gmail.com>
2021-06-15 15:24:31 +0900

    ~p.317 원인불명 테스트 통과못하는 것은 주석처리함.

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,7 +1,10 @@
 import LoginPage from '@/views/LoginPage'
+import RegisterPage from '@/views/RegisterPage'
 import Vue from 'vue'
 import Router from 'vue-router'
+
 Vue.use(Router)
+
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -9,5 +12,9 @@ export default new Router({
     path: '/login',
     name: 'LoginPage',
     component: LoginPage
+  }, {
+    path: '/register',
+    name: 'RegisterPage',
+    component: RegisterPage
   }]
 })
